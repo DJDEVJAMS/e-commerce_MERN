6b3267629ec959fce2960451a459adb8732dff58
DJDEVJAMS <alex.art.brown@gmail.com>
2024-09-12 17:48:19 -0500

    update for render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+
 
 mongoose.connect(
-  process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/progetterDB'
+  process.env.MONGO_URI 
 );
 module.exports = mongoose.connection;
