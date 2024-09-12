6fa2ace6dbe9760a74176c675594572aef64aa2d
DJDEVJAMS <alex.art.brown@gmail.com>
2024-09-12 17:54:53 -0500

    update for render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(
-  process.env.MONGO_URI 
-);
+mmongoose.createConnection()
+mongoose.connect(process.env.MONGODB_URI);
+
 module.exports = mongoose.connection;
